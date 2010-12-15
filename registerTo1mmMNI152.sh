7c47d03443e28e221715389431429ad4296c70ba
stowler <stowler@localhost>
2010-12-14 23:27:59 -0500

    registerToMNI152.sh: reverted back to style of -c cluster.nii and -b buck.nii

--- registerTo1mmMNI152.sh
+++ registerTo1mmMNI152.sh
@@ -4,7 +4,7 @@
 # USAGE:           (see fxnPrintUsage() function below)
 #
 # CREATED:	   201008?? by stowler@gmail.com http://brainwhere.googlecode.com
-# LAST UPDATED:	   20101206 by stowler@gmail.com
+# LAST UPDATED:	   20101214 by stowler@gmail.com
 #
 # DESCRIPTION:
 # Registers T1 to the 1mm MNI152 template, along with optional lesion mask deweighting and application of transform to EPI
@@ -28,16 +28,19 @@
 fxnPrintUsage() {
    #EDITME: customize for each script:
    echo >&2 "$0 - a script to register unstriped T1, lesion mask, EPI, and EPI-registered volumes (buck, etc.) to 1mmMNI152 space"
-   echo >&2 "Usage: registerTo1mmMNI152.sh      \\"
-   echo >&2 "  -s <subjectID>                   \\"
-   echo >&2 "  -t <t1.nii>                      \\"
-   echo >&2 "  -o <FullPathToOutdir>            \\"
-   echo >&2 "[ -l <lesion.nii>                  \\ ]"
-   echo >&2 "[ -e <epi.nii>                     \\ ]"
-   echo >&2 "[ <imageRegisteredToAboveEPI.nii>  \\ ]"
-   echo >&2 "[ <aSecondEPIregisteredImage.nii>  \\ ]"
-   echo >&2 "[ <aThirdEPIregisteredImage.nii>   \\ ]"
-   echo >&2 "(and so on, listing as many volumes as you like after epi.nii, as long as they've been preregistered to epi.nii)"
+   echo >&2 ""
+   echo >&2 "Usage: registerTo1mmMNI152.sh                                 \\"
+   echo >&2 "  -s <subjectID>                                              \\"
+   echo >&2 "  -t <t1.nii>                                                 \\"
+   echo >&2 "  -o <FullPathToOutdir>                                       \\"
+   echo >&2 "[ -l <lesion.nii>                                             \\ ]"
+   echo >&2 "[ -e <epi.nii>                                                \\ ]"
+   echo >&2 "[ -c <clusterMasksRegisteredToAboveEPI.nii>                   \\ ]"
+   echo >&2 "[ -c <anotherEPIregisteredCusterMask.nii>                     \\ ]"
+   echo >&2 "[ -b <buckFileOrOtherDecimalValueImage.nii>                   \\ ]"
+   echo >&2 "[ -b <anotherEPIregisteredBuckOrOtherDecimalValueImage.nii>     ]"
+   echo >&2 ""
+   echo >&2 "(specify as many -b and -c files as you like, just prepend EVERY file with -b or -c)"
 }
 
 
@@ -87,9 +90,11 @@ t1=""
 outDir=""
 lesion=""
 epi=""
+integerVolumes=""
+decimalVolumes=""
 
 # argument processing with getopt:
-set -- `getopt s:t:o:l:e: "$@"`
+set -- `getopt s:t:o:l:e:c:b: "$@"`
 [ $# -lt 1 ] && exit 1	# getopt failed
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -98,6 +103,8 @@ while [ $# -gt 0 ]; do
       -o)   outDir="${2}"; shift ;;
       -l)   lesion="${2}"; shift ;;
       -e)   epi="${2}"; shift ;;
+      -c)   integerVolumes="${integerVolumes} ${2}"; shift ;;
+      -b)   decimalVolumes="${decimalVolumes} ${2}"; shift ;;
       --)   shift; break ;;
       -*)   echo >&2 "usage: $0 - TBD: a short usage note "; exit 1 ;;
        *)   break ;; # terminate while loop
@@ -164,7 +171,27 @@ if [ ! -z ${epi} ]; then
 	fi
 fi
 
-for image in "$@"; do
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws"
+for image in `echo ${integerVolumes}`; do
+	#echo "DEBUG: integerVolumes=${integerVolumes}"
+	#read
+	if [ ! -z ${image} ]; then
+		fxnValidateImages ${image}
+		if [ $? -eq 1 ]; then 
+			echo ""
+			echo "ERROR: $image is not a valid image"
+			echo ""
+			fxnPrintUsage
+			echo ""
+			exit 1
+		#else
+		#	echo "DEBUG $image is a valid image. Yay!"
+		fi
+	fi
+done
+
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws"
+for image in `echo ${decimalVolumes}`; do
 	if [ ! -z ${image} ]; then
 		fxnValidateImages ${image}
 		if [ $? -eq 1 ]; then 
@@ -220,7 +247,8 @@ echo "Images to nonlinear register into 1mmMNI152"
 echo "-IMPORTANT: a lesion must match T1's geometry"
 echo "-IMPORTANT: images following EPI must match EPI geometry"
 sh ${bwDir}/displayImageGeometry.sh -n ${t1} >> ${tempDir}/inputUnformatted.txt
-for image in $t1 $lesion $epi "${@}"; do
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws:
+for image in $t1 $lesion $epi `echo ${integerVolumes} ${decimalVolumes}`; do
 	if [ -s $image ]; then
 		sh ${bwDir}/displayImageGeometry.sh -r $image >> ${tempDir}/inputUnformatted.txt
 	fi
@@ -256,7 +284,8 @@ if [ -s "`echo ${epi}`" ]; then
         -inset ${epi}
 	ls -1 ${tempDir}/${blind}_epi*
 fi
-for image in "${@}"; do
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws:
+for image in `echo ${integerVolumes} ${decimalVolumes}`; do
         if [ -s "`echo ${image}`" ]; then
                 imageBasename="`echo ${image} | xargs basename | xargs ${FSLDIR}/bin/remove_ext`"
                 #echo "DEBUG imageBasename is ${imageBasename}"
@@ -344,7 +373,7 @@ fi
 # calculation of nonlinear t1->mni transformation:
 echo ""
 echo ""
-echo "nonlinear transformation of T1 to template takes about 15 minutes..."
+echo "nonlinear transformation of t1 to template takes about 15 minutes..."
 echo "(ignore messages about requested tolerance...unless your transformation turns out horrible, in which case they may have been meaningful)"
 echo ""
 # include -inmask if we have a lesion, don't if we don't: 
@@ -420,7 +449,29 @@ if [ -s "`echo ${epi}`" ]; then
 	ls -l ${tempDir}/${blind}_epi_warped*
 fi
 
-for image in "${@}"; do
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws:
+# integer-based volumes like cluster masks are registered with nearest neighbor interpolation:
+for image in `echo ${integerVolumes}`; do
+	if [ -s ${image} ]; then
+		echo ""
+		echo ""
+		imageBasename="`echo ${image} | xargs basename | xargs ${FSLDIR}/bin/remove_ext`"
+		echo "DEBUG imageBasename is ${imageBasename}"
+		echo "applying nonlinear warp to ${imageBasename} (probably 30 or fewer minutes)..."
+		applywarp \
+		--ref=${FSLDIR}/data/standard/MNI152_T1_1mm \
+		--in=${tempDir}/${imageBasename} \
+		--warp=${tempDir}/${blind}_nonlinear_transf \
+		--premat=${tempDir}/${blind}_funct2struct.mat \
+		--out=${tempDir}/${imageBasename}_warped.nii.gz \
+		--interp=nn
+		ls -l ${tempDir}/${imageBasename}_warped*
+	fi
+done
+
+# the following requires echo $var, not just $var for ws-sep'd values in $var to be subsequently read as multiple values instead of single value containing ws:
+# decimal-based volumes like stats are registered with fancier interpolation:
+for image in `echo ${decimalVolumes}`; do
 	if [ -s "`echo ${image}`" ]; then
 		echo ""
 		echo ""
@@ -432,7 +483,8 @@ for image in "${@}"; do
 		--in=${tempDir}/${imageBasename} \
 		--warp=${tempDir}/${blind}_nonlinear_transf \
 		--premat=${tempDir}/${blind}_funct2struct.mat \
-		--out=${tempDir}/${imageBasename}_warped.nii.gz
+		--out=${tempDir}/${imageBasename}_warped.nii.gz \
+		--interp=trilinear
 		ls -l ${tempDir}/${imageBasename}_warped*
 	fi
 done
